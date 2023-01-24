a3eb28300a366d5ccf05f2948cc41c2f38ff4da2
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-01-23 22:55:31 -0300

    version01

---
+++ helloworld/src/main/java/com/helloworld/helloworld/controller/BSMController.java
@@ -0,0 +1,21 @@
+package com.helloworld.helloworld.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+	@RestController
+	@RequestMapping("/bsm")
+
+public class BSMController {
+	 @GetMapping
+	    public List<String> getBSMs() {
+	        List<String> bsms = Arrays.asList("Comunicação", "Mentalidade de Crescimento", "Orientação ao Detalhe", "Orientação ao Futuro", "Persistência", "Proatividade", "Responsabilidade Pessoal", "Trabalho em Equipe");
+	        return bsms;
+	    }
+}
---
+++ helloworld/src/main/java/com/helloworld/helloworld/controller/ObjetivosController.java
@@ -0,0 +1,22 @@
+package com.helloworld.helloworld.controller;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+	@RestController
+	@RequestMapping("/objetivos")
+public class ObjetivosController {
+		
+		@GetMapping
+		public List<String> getobjetivos() {
+	        List<String> objetivos = Arrays.asList("Comunicação", " Trabalho em Equipe", " e ", " Orientação ao Futuro");
+	        return objetivos;
+		}
+	
+	
+
+}
